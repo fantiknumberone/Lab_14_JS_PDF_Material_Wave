1ae86e38f04b6fcfb54d513c08490a465ceab765
fantiknumberone <fantiknumberone@gmail.com>
2024-12-04 01:40:35 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,53 +4,6 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
 
-document.querySelector("#download").addEventListener("click", function () {
-    // Преобразование текущей страницы в PDF
-    html2pdf()
-        .from(document.body)
-        .save("resume.pdf");
-});
-
-// Сохранение состояния редактируемых полей
-const editableElements = document.querySelectorAll('.editable');
-editableElements.forEach(element => {
-    element.addEventListener('input', function () {
-        localStorage.setItem(element.innerText.substr(0, 10), element.innerText);
-    });
-});
-
-// Загрузка сохраненных данных
-window.onload = () => {
-    editableElements.forEach(element => {
-        const savedText = localStorage.getItem(element.innerText.substr(0, 10));
-        if (savedText) {
-            element.innerText = savedText;
-        }
-    });
-};
-
-// Добавление эффекта "Material Wave"
-document.querySelectorAll(".ripple").forEach(button => {
-    button.addEventListener('click', function (e) {
-        const ripple = document.createElement("span");
-        const rect = this.getBoundingClientRect();
-        const size = Math.max(rect.width, rect.height);
-        const x = e.clientX - rect.left - size / 2;
-        const y = e.clientY - rect.top - size / 2;
-
-        ripple.style.width = ripple.style.height = `${size}px`;
-        ripple.style.left = `${x}px`;
-        ripple.style.top = `${y}px`;
-        ripple.classList.add("ripple-effect");
-
-        this.appendChild(ripple);
-        setTimeout(() => {
-            ripple.remove();
-        }, 600);
-    });
-});
-
-
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
